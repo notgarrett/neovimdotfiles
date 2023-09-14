ad80d52a2235e09f3b476f332a857bc4d552f728
Garrett Giles <garrettrgiles@gmail.com>
2023-09-14 15:56:51 -0400

    Some fixes

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -1,15 +1,6 @@
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
-    return
+  return
 end
-local list = {
 
-}
-
-nvim_tree.setup {
-    view = {
-        mappings = {
-            list = list
-        }
-    }
-}
+nvim_tree.setup()
